57e49b25095806b9b90e0e33dea6cf3f478461bc
contorax <contorax@yandex.com>
2018-12-22 14:55:42 -0700

    create auxiliary class Contact
    create class Contact and added new methods and comments
    selected auxiliary methods for test_add_new_Contact

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+
+    def __init__(self, first_name, last_name, mobile_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.mobile_number = mobile_number
+        self.email = email
--- test_add_contact.py
+++ test_add_contact.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
-
+from contact import Contact
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -17,30 +17,58 @@ class TestAddContact(unittest.TestCase):
     
     def test_add_contact(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.create_contact(driver,  Contact (first_name = "JOKhan", last_name="Whitwad", mobile_number="3919999992", email="adaiy@company.com"))
+        self.return_to_home_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_contact(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.create_contact(driver,Contact (first_name="", last_name="", mobile_number="", email="x@y.com"))
+        self.return_to_home_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, driver):
+        driver.find_element_by_link_text("home").click()
+
+    def create_contact(self, driver, contact):
+        # init contact creation
         driver.find_element_by_link_text("add new").click()
+        # fill contact form
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("Adam Khan")
+        driver.find_element_by_name("firstname").send_keys(contact.first_name)
         driver.find_element_by_name("lastname").click()
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("White Aswad")
+        driver.find_element_by_name("lastname").send_keys(contact.last_name)
         driver.find_element_by_name("home").click()
         driver.find_element_by_name("mobile").click()
         driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("3915653412")
+        driver.find_element_by_name("mobile").send_keys(contact.mobile_number)
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("adam-iy@company.com")
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
-        driver.find_element_by_link_text("home").click()
-        driver.find_element_by_link_text("Logout").click()
-    
+        driver.find_element_by_name("email").send_keys(contact.email)
+        # submit contact creation
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
